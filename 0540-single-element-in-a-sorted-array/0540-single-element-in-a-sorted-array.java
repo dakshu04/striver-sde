033dde3efa14cfe5223ce4f20ef7c650c2701b14
Daksh Purohit <dakshgrows@gmail.com>
2025-02-24 19:23:53 +0530

    Time: 0 ms (100%), Space: 49.5 MB (99.91%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,14 +1,30 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-        //TC->O(n),SC->O(n)
-        HashMap<Integer,Integer> map = new HashMap<>();
         int n = nums.length;
-        for(int i = 0; i < n; i++) {
-            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        int left = 0;
+        int right  = nums.length - 1;
+        //Edge Case of only 1 length of nums
+        if(nums.length == 1) {
+            return nums[0];
         }
-        for(int num :  map.keySet()) {
-            if(!(map.get(num) > 1)) {
-                return num;
+        //[1,1,2] - Edge Case 2       
+        if(nums[n-1] != nums[n-2]) {
+            return nums[n-1];
+        }
+        //[1,2,2,3,3] - Edge Case 3
+        if(nums[0] != nums[1]) {
+            return nums[0];
+        }
+        while(left <= right) {
+            int mid = left + (right - left) / 2;
+            if(nums[mid] != nums[mid + 1] && nums[mid] != nums[mid - 1]) {
+                return nums[mid];
+            }
+            if((mid % 2 == 0 && nums[mid] == nums[mid + 1]) || (mid % 2 == 1 && nums[mid] == nums[mid - 1])) {
+                left = mid + 1;
+            }
+            else {
+                right = mid - 1;
             }
         }
         return -1;
