e6994366937a0db44b68b442c8031f8f7c1741a9
Daksh Purohit <dakshgrows@gmail.com>
2025-02-26 17:08:15 +0530

    Time: 2 ms (59.78%), Space: 45.4 MB (99.7%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -2,28 +2,48 @@ class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         int n1 = nums1.length;
         int n2 = nums2.length;
+        int cnt = 0;
+        //TC->O(n+m),SC->O(1)
         int m = n1 + n2;
-        int[] arr = new int[m];
-        int i = 0;
-        int j = 0;
-        int k = 0;
+        int idx1 = -1;
+        int idx2 = -1;
+        if(m % 2 == 1)  {
+            idx1 = m / 2;
+        } else {
+            idx1 = m / 2 - 1;
+            idx2 = m / 2;
+        }
+        int idx1El = -1;
+        int idx2El = -1;
+        int i = 0, j = 0;
         while(i < n1 && j < n2) {
             if(nums1[i] < nums2[j]) {
-                arr[k++] = nums1[i++];
+                if(cnt == idx1) idx1El = nums1[i];
+                if(cnt == idx2) idx2El = nums1[i];
+                cnt++;
+                i++;
             } else {
-                arr[k++] = nums2[j++];
+                if(cnt == idx1) idx1El = nums2[j];
+                if(cnt == idx2) idx2El = nums2[j];
+                cnt++;
+                j++;
             }
         }
         while(i < n1) {
-            arr[k++] = nums1[i++];
+            if(cnt == idx1) idx1El = nums1[i];
+            if(cnt == idx2) idx2El = nums1[i];
+            cnt++;
+            i++;
         }
         while(j < n2) {
-            arr[k++] = nums2[j++];
+            if(cnt == idx1) idx1El = nums2[j];
+            if(cnt == idx2) idx2El = nums2[j];
+            cnt++;
+            j++;
         }
-        // Calculate median
-        if (m % 2 == 1) {
-            return arr[m / 2]; // Odd length: return middle element
+        if(m % 2 == 1) {
+            return idx1El;
         }
-        return (arr[m / 2] + arr[(m / 2) - 1]) / 2.0; // Even length: average of two middle elements
+        return (idx1El + idx2El) / 2.0;
     }
 }
