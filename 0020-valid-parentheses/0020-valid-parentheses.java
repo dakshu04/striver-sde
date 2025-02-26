daffa561993587caf17280941c769efba9609a55
Daksh Purohit <dakshgrows@gmail.com>
2025-02-26 18:06:04 +0530

    Time: 2 ms (97.68%), Space: 41.9 MB (47.43%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,28 +1,21 @@
-import java.util.Stack;
-
 class Solution {
-    public boolean isValid(String str) {
-        Stack<Character> s = new Stack<>();
-        
-        for (int i = 0; i < str.length(); i++) {
-            char ch = str.charAt(i);
-            
-            if (ch == '(' || ch == '{' || ch == '[') {
-                s.push(ch);
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for(int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if(ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch);
             } else {
-                // Closing brackets case
-                if (s.isEmpty()) {
+                if(stack.isEmpty()) {
                     return false;
                 }
-                if ((s.peek() == '(' && ch == ')') || (s.peek() == '{' && ch == '}') || (s.peek() == '[' && ch == ']')) {
-                    s.pop(); // Matched, pop the corresponding opening bracket
+                if((stack.peek() == '(' && ch == ')') || (stack.peek() == '{' && ch == '}') || (stack.peek() == '[' && ch ==']')) {
+                    stack.pop();
                 } else {
-                    return false; // Mismatched brackets
+                    return false;
                 }
             }
         }
-        
-        // Check if there are remaining opening brackets
-        return s.isEmpty();
+        return stack.isEmpty();
     }
 }
