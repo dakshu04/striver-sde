cf346c9372a800c231f461165179dbe2f603554e
Daksh Purohit <dakshgrows@gmail.com>
2025-03-03 23:41:15 +0530

    Time: 0 ms (100%), Space: 44.8 MB (59.1%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,16 +1,28 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-       //TC->O(n),SC->O(n)
-        //Use LinkedHashSet to have ans in ascending order
-        int n = nums.length;
-        LinkedHashSet<Integer> hs = new LinkedHashSet<>();
-        for(int i = 0; i < n; i++) {
-            hs.add(nums[i]);
-        }
-        int idx = 0;
-        for(int num : hs) {
-            nums[idx++] = num;
-        }
+    //    //TC->O(n),SC->O(n)
+    //     //Use LinkedHashSet to have ans in ascending order
+    //     int n = nums.length;
+    //     LinkedHashSet<Integer> hs = new LinkedHashSet<>();
+    //     for(int i = 0; i < n; i++) {
+    //         hs.add(nums[i]);
+    //     }
+    //     int idx = 0;
+    //     for(int num : hs) {
+    //         nums[idx++] = num;
+    //     }
+    //     return idx;
+
+    //      TC->O(n),SC->O(1);
+            int ele = nums[0];
+            int n = nums.length;
+            int idx = 1;
+            for(int i = 1; i < n; i++) {
+                if(nums[i] != ele) {
+                    nums[idx++] = nums[i];
+                    ele = nums[i];
+                }
+            }
         return idx;
     }
 }
