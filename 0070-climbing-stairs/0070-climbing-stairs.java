c44a83da41036a07ca2e4c87681530edc359c75a
Daksh Purohit <dakshgrows@gmail.com>
2025-03-25 13:52:10 +0530

    Time: 0 ms (100%), Space: 40.1 MB (96.56%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,15 +1,17 @@
 class Solution {
     public int climbStairs(int n) {
-        if(n == 0 || n == 1) {
+        if (n == 0 || n == 1) {
             return 1;
         }
-        int prev = 1; 
-        int curr = 1;
-        for(int i = 2; i <= n; i++) {
-            int temp = curr;
-            curr = prev + curr;
-            prev = temp;
+        //TC->O(N),SC->O(N)
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
         }
-        return curr;
+        
+        return dp[n];
     }
 }
