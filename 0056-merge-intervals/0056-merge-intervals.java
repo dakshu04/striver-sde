9831f3e00ea1686d1ab146e53f5a89a49d277f30
Daksh Purohit <dakshgrows@gmail.com>
2025-03-26 16:19:17 +0530

    Time: 9 ms (49.03%), Space: 46.9 MB (25.26%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,23 +1,21 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
         int n = intervals.length;
-        if (n <= 1) return intervals; // Edge case: No merging needed
-        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);//ascemding
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
         int start = intervals[0][0];
-        int end = intervals[0][1];
+        int end = intervals[0][1]; 
         int[][] ans = new int[n][2];
         int idx = 0;
         for(int i = 1; i < n; i++) {
-            if(intervals[i][0] <= end) {
-                end = Math.max(end, intervals[i][1]);
+            if(end >= intervals[i][0]) {
+                end = Math.max(intervals[i][1], end);
             } else {
                 ans[idx++] = new int[]{start, end};
                 start = intervals[i][0];
                 end = intervals[i][1];
             }
         }
-        // Step 3: Store the last merged interval
         ans[idx++] = new int[]{start, end};
-        return Arrays.copyOf(ans, idx); //only till idx will return
+        return Arrays.copyOf(ans, idx);
     }
 }
