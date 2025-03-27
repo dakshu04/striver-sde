73fb53d335446631e75960529a87f680a712cfcb
Daksh Purohit <dakshgrows@gmail.com>
2025-03-27 22:43:47 +0530

    Time: 0 ms (100%), Space: 40.5 MB (46.65%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,24 +1,20 @@
 class Solution {
-    public int f(int i, int j, int[][] dp) {
-        //TC->O(N*M), SC->O(N-1+M-1)+O(M*N)
-        if(i == 0 && j == 0) {
-            return 1;
-        }
-        if(i < 0 || j < 0) {
-            return 0;
-        }
-        if(dp[i][j] != -1) {
-            return dp[i][j];
-        }
-        int up = f(i - 1, j, dp);
-        int left = f(i, j - 1, dp);
-        return dp[i][j] = up + left;
-    }
     public int uniquePaths(int m, int n) {
+        // Create a DP table of size m x n
         int[][] dp = new int[m][n];
-        for(int[] row : dp) {
-            Arrays.fill(row, -1);
+        //TC->O(m × n) SC-> O(m × n)
+        // Base case: The first row and first column should all be 1
+        // because there's only one way to reach any of those cells (either moving right or down).
+        for (int i = 0; i < m; i++) dp[i][0] = 1;
+        for (int j = 0; j < n; j++) dp[0][j] = 1;
+
+        // Fill the DP table iteratively
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1]; // Sum of paths from top and left
+            }
         }
-        return f(m-1, n-1, dp);
+
+        return dp[m - 1][n - 1]; // Return the value at the bottom-right cell
     }
 }
