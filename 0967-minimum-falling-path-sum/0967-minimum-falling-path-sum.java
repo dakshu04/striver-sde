a0d0c8097e32f3e4f5a3c0d7123fb4a8422e380a
Daksh Purohit <dakshgrows@gmail.com>
2025-03-28 12:05:22 +0530

    Time: 7 ms (58.42%), Space: 45.4 MB (62.68%) - LeetHub

--- 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.java
+++ 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.java
@@ -1,36 +1,28 @@
-import java.util.*;
-
 class Solution {
     public static int minFallingPathSum(int[][] matrix) {
         int n = matrix.length;
         int m = matrix[0].length;
-
-        int[][] dp = new int[n][m];
-
+        int[] prev = new int[m]; // Store previous row results
         // Base Case: First row is copied as it is
         for (int j = 0; j < m; j++) {
-            dp[0][j] = matrix[0][j];
+            prev[j] = matrix[0][j];
         }
-
-        // Bottom-up DP filling
         for (int i = 1; i < n; i++) {
+            int[] curr = new int[m];
             for (int j = 0; j < m; j++) {
-                int up = matrix[i][j] + dp[i - 1][j]; // Up
-                int leftDiagonal = matrix[i][j] + (j > 0 ? dp[i - 1][j - 1] : (int) 1e9); // Left diagonal
-                int rightDiagonal = matrix[i][j] + (j < m - 1 ? dp[i - 1][j + 1] : (int) 1e9); // Right diagonal
+                int up = matrix[i][j] + prev[j]; // Up
+                int leftDiagonal = matrix[i][j] + (j > 0 ? prev[j - 1] : (int) 1e9);
+                int rightDiagonal = matrix[i][j] + (j < m - 1 ? prev[j + 1] : (int) 1e9);
 
-                // Take minimum of all possible paths
-                dp[i][j] = Math.min(up, Math.min(leftDiagonal, rightDiagonal));
+                curr[j] = Math.min(up, Math.min(leftDiagonal, rightDiagonal));
             }
+            prev = curr; // Update previous row
         }
-
         // Find the minimum in the last row
         int minSum = Integer.MAX_VALUE;
         for (int j = 0; j < m; j++) {
-            minSum = Math.min(minSum, dp[n - 1][j]);
+            minSum = Math.min(minSum, prev[j]);
         }
-
         return minSum;
     }
-
 }
