c4aa1983904209ebedcfe7e95b3c72bda26b7818
Daksh Purohit <dakshgrows@gmail.com>
2025-03-28 10:50:41 +0530

    Time: 4 ms (62.58%), Space: 47.5 MB (69.82%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -1,18 +1,19 @@
-class Solution {//TC->O(n*m),SC->O(n*m), No rec tech stack
+class Solution {//TC->O(n*m),SC->O(1)
     public int minPathSum(int[][] grid) {
         int n = grid.length, m = grid[0].length;
-        int[][] dp = new int[n][m];
-        // Fill the dp table
+        int[] prev = new int[m]; // Stores previous row results
         for (int i = 0; i < n; i++) {
+            int[] curr = new int[m]; // Current row being computed
             for (int j = 0; j < m; j++) {
-                if (i == 0 && j == 0) dp[i][j] = grid[i][j]; // Base case (starting cell)
+                if (i == 0 && j == 0) curr[j] = grid[i][j]; // Base case
                 else {
-                    int up = (i > 0) ? dp[i - 1][j] : (int) 1e9; // Value from top cell
-                    int left = (j > 0) ? dp[i][j - 1] : (int) 1e9; // Value from left cell
-                    dp[i][j] = grid[i][j] + Math.min(up, left);
+                    int up = (i > 0) ? prev[j] : (int) 1e9; // Value from the row above
+                    int left = (j > 0) ? curr[j - 1] : (int) 1e9; // Value from left cell
+                    curr[j] = grid[i][j] + Math.min(up, left);
                 }
             }
+            prev = curr; // Update prev row to the current row
         }
-        return dp[n - 1][m - 1]; // Final answer at the bottom-right cell
+        return prev[m - 1]; // Answer is in the last column of the last processed row
     }
 }
