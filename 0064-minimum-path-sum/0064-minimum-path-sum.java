1fb97cc3dc008fe0b1bf5c45a2abfa953cbd3e32
Daksh Purohit <dakshgrows@gmail.com>
2025-03-28 10:49:21 +0530

    Time: 5 ms (28.39%), Space: 47.6 MB (69.82%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -1,14 +1,18 @@
-class Solution {
-    public int f(int i, int j, int[][] grid) {
-        if(i == 0 && j == 0) return grid[0][0];
-        if(i < 0 || j < 0) return Integer.MAX_VALUE; //maxValue so ans would not include it as it is asking for minValue
-        int up = i > 0 ? grid[i][j] + f(i - 1, j, grid) : Integer.MAX_VALUE;
-        int left = j > 0 ? grid[i][j] + f(i, j - 1, grid) : Integer.MAX_VALUE;
-        return Math.min(up, left);
-    }
+class Solution {//TC->O(n*m),SC->O(n*m), No rec tech stack
     public int minPathSum(int[][] grid) {
-        int n = grid.length;
-        int m = grid[0].length;
-        return f(n - 1, m - 1, grid);
+        int n = grid.length, m = grid[0].length;
+        int[][] dp = new int[n][m];
+        // Fill the dp table
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (i == 0 && j == 0) dp[i][j] = grid[i][j]; // Base case (starting cell)
+                else {
+                    int up = (i > 0) ? dp[i - 1][j] : (int) 1e9; // Value from top cell
+                    int left = (j > 0) ? dp[i][j - 1] : (int) 1e9; // Value from left cell
+                    dp[i][j] = grid[i][j] + Math.min(up, left);
+                }
+            }
+        }
+        return dp[n - 1][m - 1]; // Final answer at the bottom-right cell
     }
 }
