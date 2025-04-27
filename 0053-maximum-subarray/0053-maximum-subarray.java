aecbf0903946f65c5da1bd7ee229286d53f8b585
Daksh Purohit <dakshgrows@gmail.com>
2025-04-27 10:53:29 +0530

    Time: 1 ms (99.54%), Space: 57.4 MB (21.37%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,13 +1,14 @@
 class Solution {
     public int maxSubArray(int[] nums) {
         int n = nums.length;
-        int maxSum = nums[0];
-        int currSum = nums[0];
-        //TC->O(n^1),SC->O(1)
-        //Kadanes Algo
-        for(int i = 1; i < n; i++) {
-            currSum = Math.max(currSum + nums[i], nums[i]);
-            maxSum = Math.max(maxSum, currSum);
+        int sum = 0;
+        int maxSum = Integer.MIN_VALUE;
+        for(int i = 0; i < n; i++) {
+            sum += nums[i];
+            maxSum = Math.max(sum, maxSum);
+            if(sum < 0) {
+                sum = 0;
+            }
         }
         return maxSum;
     }
