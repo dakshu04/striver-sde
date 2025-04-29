3e709ed3b3e43cf8b1489e2ad70117e3b520a94e
Daksh Purohit <dakshgrows@gmail.com>
2025-04-30 00:31:19 +0530

    Time: 0 ms (100%), Space: 43.2 MB (79.09%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -13,13 +13,13 @@
  *     }
  * }
  */
-class Solution {//TC->O(n),SC->O(1)
+class Solution {
+    public boolean isValid(TreeNode root, long min, long max) {
+        if(root == null) return true;
+        if(root.val <= min || root.val >= max) return false;
+        return isValid(root.left, min, root.val) && isValid(root.right, root.val, max);
+    }
     public boolean isValidBST(TreeNode root) {
         return isValid(root, Long.MIN_VALUE, Long.MAX_VALUE);
-    }
-    public boolean isValid(TreeNode root, int minValue, int maxValue) {
-        if(root == null) return true;
-        if(root.val <= minValue || root.val >= maxValue) return false;
-        return isValid(root.left, minValue, root.val) && isValid(root.right, root.val, maxValue);
     }
 }
