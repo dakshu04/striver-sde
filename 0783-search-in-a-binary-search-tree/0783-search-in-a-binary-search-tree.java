95a60f3888a6d2071275973117f5ce5d64ab1e34
Daksh Purohit <dakshgrows@gmail.com>
2025-04-30 00:03:48 +0530

    Time: 0 ms (100%), Space: 45.2 MB (71.77%) - LeetHub

--- 0783-search-in-a-binary-search-tree/0783-search-in-a-binary-search-tree.java
+++ 0783-search-in-a-binary-search-tree/0783-search-in-a-binary-search-tree.java
@@ -13,17 +13,13 @@
  *     }
  * }
  */
-class Solution {//TC->O(N){skewed},O(logn){balanced},SC->O(logn){balanced},O(N){skewed}
+class Solution {
     public TreeNode searchBST(TreeNode root, int val) {
-        if(root == null) {
-            return root;
-        }
-        if(root.val == val) {
-            return root;
-        }
+        if(root == null) return null;
+        if(root.val == val) return root;
         if(val > root.val) {
             return searchBST(root.right, val);
-        } else{
+        } else {
             return searchBST(root.left, val);
         }
     }
