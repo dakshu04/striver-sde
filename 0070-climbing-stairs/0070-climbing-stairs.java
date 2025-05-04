9e1081b17f5c06dcd5f68d181e421a37ba81fb9f
Daksh Purohit <dakshgrows@gmail.com>
2025-05-04 15:11:33 +0530

    Time: 0 ms (100%), Space: 40.1 MB (96.79%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,25 +1,12 @@
 class Solution {
-    public int helper(int n, int[] dp) {
-        if(n == 0 || n == 1) return 1;
-        if(dp[n] != -1) {
-            return dp[n];
-        }
-        int prev = helper(n - 1, dp);
-        int prev2 = helper(n - 2, dp);
-        dp[n] = prev + prev2;
-        return dp[n];
-    }
     public int climbStairs(int n) {
-        // if(n == 0 || n == 1) {//TC->O(2^n)
-        //     return 1;
-        // }
-        // int left = climbStairs(n - 1);
-        // int right = climbStairs(n - 2);
-        // return left + right;
-
-        //Memoization-TC->O(n),SC->O(n)
+        if(n == 0 || n == 1) return 1;
         int[] dp = new int[n + 1];
-        Arrays.fill(dp, -1);
-        return helper(n, dp);
+        dp[0] = 1;
+        dp[1] = 1;
+        for(int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
     }
 }
