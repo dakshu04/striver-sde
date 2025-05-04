e36078ba2b4240c3c0d2cc9344b3505b329371ee
Daksh Purohit <dakshgrows@gmail.com>
2025-05-04 16:44:45 +0530

    Time: 1 ms (38.14%), Space: 42.7 MB (98.04%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.java
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.java
@@ -1,17 +1,13 @@
 class Solution {
-    //TC->O(n), SC->O(n) - Memoization
-    public int dfs(int[] cost, int idx, int[] dp) {
-        if(idx == 0) return cost[0]; //can start with both 0 or 1
-        if(idx == 1) return cost[1]; //can start with both 0 or 1
-        if(dp[idx] != -1) return dp[idx];
-        int left = cost[idx] + dfs(cost, idx - 1, dp);
-        int right = cost[idx] + dfs(cost, idx - 2, dp);
-        return dp[idx] = Math.min(left, right);
-    }
+    //TC->O(n), SC->O(n) - Tabulation
     public int minCostClimbingStairs(int[] cost) {
         int n = cost.length;
         int[] dp = new int[n];
-        Arrays.fill(dp, -1);
-        return Math.min(dfs(cost, n - 1, dp), dfs(cost, n - 2, dp));
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+        for(int i = 2; i < cost.length; i++) {
+            dp[i] = Math.min(dp[i - 1], dp[i - 2]) + cost[i];
+        }
+        return Math.min(dp[n - 1], dp[n - 2]); //can start both 1 and 2
     }
 }
