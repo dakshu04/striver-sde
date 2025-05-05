7e98b53ee44bf821beaab5231a0de290f8eb45e5
Daksh Purohit <dakshgrows@gmail.com>
2025-05-05 10:07:36 +0530

    Time: 0 ms (100%), Space: 40.9 MB (90.99%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,23 +1,19 @@
 class Solution {
-    private int solve(int start, int end, int[] nums) {
-        int prev = nums[start];
-        int prev2 = 0;
-        for (int i = start + 1; i <= end; i++) {
-            int pick = nums[i];
-            if (i > start + 1) {
-                pick += prev2;
-            }
-            int nonPick = prev;
-            int curr = Math.max(pick, nonPick);
-            prev2 = prev;
-            prev = curr;
-        }
-        return prev;
+    //TC->O(N), SC->O(N)(Memo)
+    public int dfs(int start, int end, int[] nums, int[] dp) {
+        if(start > end) return 0;
+        if(dp[start] != -1) return dp[start];
+        int pick = nums[start] + dfs(start + 2, end, nums, dp);
+        int notPick = dfs(start + 1, end, nums, dp);
+        return dp[start] = Math.max(pick, notPick);
     }
     public int rob(int[] nums) {
         int n = nums.length;
-        if (n == 1) return nums[0];
-        // Solve once excluding the first house, and once excluding the last house
-        return Math.max(solve(1, n - 1, nums), solve(0, n - 2, nums));
+        if(n == 1) return nums[0];
+        int[] dp1 = new int[n];
+        int[] dp2 = new int[n];
+        Arrays.fill(dp1, -1);
+        Arrays.fill(dp2, -1);
+        return Math.max(dfs(1, n - 1, nums, dp1), dfs(0, n - 2, nums, dp2));
     }
 }
