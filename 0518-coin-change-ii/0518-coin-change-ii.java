9f73a70909815be2d6b223adb76dea32009e25e7
Daksh Purohit <dakshgrows@gmail.com>
2025-07-07 23:47:44 +0530

    Time: 3 ms (99.97%), Space: 41.6 MB (71.46%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,27 +1,14 @@
 class Solution {
     public int change(int amount, int[] coins) {
-        int n = coins.length;
-        int[][] dp = new int[n][amount + 1];
-        // Base case: amount == 0 → 1 way (no coins)
-        for (int i = 0; i < n; i++) {
-            dp[i][0] = 1;
-        }
-        // Fill first row (only coins[0])
-        for (int t = 0; t <= amount; t++) {
-            if (t % coins[0] == 0) {
-                dp[0][t] = 1;
+        int[] dp = new int[amount + 1];
+        dp[0] = 1; // 1 way to make 0
+
+        for (int coin : coins) {
+            for (int t = coin; t <= amount; t++) {
+                dp[t] += dp[t - coin];
             }
         }
-        for (int i = 1; i < n; i++) {
-            for (int t = 0; t <= amount; t++) {
-                int notTake = dp[i - 1][t];
-                int take = 0;
-                if (coins[i] <= t) {
-                    take = dp[i][t - coins[i]];
-                }
-                dp[i][t] = take + notTake;
-            }
-        }
-        return dp[n - 1][amount];
+
+        return dp[amount];
     }
 }
