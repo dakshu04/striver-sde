0c36b172b4e416db2971f05e564e67eb7edf672c
Daksh Purohit <dakshgrows@gmail.com>
2025-07-09 19:03:37 +0530

    Time: 0 ms (100%), Space: 43.1 MB (78.18%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.java
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.java
@@ -1,16 +1,12 @@
 class Solution {
-    //TC->O(n), SC->O(1) - Optimization
     public int minCostClimbingStairs(int[] cost) {
         int n = cost.length;
         int[] dp = new int[n];
-        if(n == 1) return cost[0];
-        int prev2 = cost[0];
-        int prev1 = cost[1];
-        for(int i = 2; i < cost.length; i++) {
-            int curr = cost[i] + Math.min(prev1, prev2);
-            prev2 = prev1;
-            prev1 = curr;
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+        for(int idx = 2; idx < n; idx++) {
+            dp[idx] = cost[idx] + Math.min(dp[idx-1], dp[idx-2]);
         }
-        return Math.min(prev1, prev2);
+        return Math.min(dp[n-1], dp[n-2]);
     }
 }
