6fce03910b7a565b83f31257a9b7f6daa69800ba
Daksh Purohit <dakshgrows@gmail.com>
2025-07-13 16:07:56 +0530

    Time: 7 ms (57.11%), Space: 45.4 MB (51.54%) - LeetHub

--- 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.java
+++ 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.java
@@ -1,28 +1,28 @@
 class Solution {
-    public static int minFallingPathSum(int[][] matrix) {
+    public int minFallingPathSum(int[][] matrix) {
         int n = matrix.length;
         int m = matrix[0].length;
-        int[] prev = new int[m]; // Store previous row results
-        // Base Case: First row is copied as it is
+        // Create DP table
+        int[][] dp = new int[n][m];
+        // Base case: first row of dp is same as matrix
         for (int j = 0; j < m; j++) {
-            prev[j] = matrix[0][j];
+            dp[0][j] = matrix[0][j];
         }
+        // Fill the dp table row by row
         for (int i = 1; i < n; i++) {
-            int[] curr = new int[m];
             for (int j = 0; j < m; j++) {
-                int up = matrix[i][j] + prev[j]; // Up
-                int leftDiagonal = matrix[i][j] + (j > 0 ? prev[j - 1] : (int) 1e9);
-                int rightDiagonal = matrix[i][j] + (j < m - 1 ? prev[j + 1] : (int) 1e9);
+                int up = dp[i - 1][j];
+                int leftDia = (j > 0) ? dp[i - 1][j - 1] : (int) 1e9;
+                int rightDia = (j < m - 1) ? dp[i - 1][j + 1] : (int) 1e9;
 
-                curr[j] = Math.min(up, Math.min(leftDiagonal, rightDiagonal));
+                dp[i][j] = matrix[i][j] + Math.min(up, Math.min(leftDia, rightDia));
             }
-            prev = curr; // Update previous row
         }
-        // Find the minimum in the last row
-        int minSum = Integer.MAX_VALUE;
+        // Answer is the min of the last row
+        int mini = Integer.MAX_VALUE;
         for (int j = 0; j < m; j++) {
-            minSum = Math.min(minSum, prev[j]);
+            mini = Math.min(mini, dp[n - 1][j]);
         }
-        return minSum;
+        return mini;
     }
 }
