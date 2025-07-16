b53e59d724989c5f6a7cdcb46c4cdd8be6c5c5ca
Daksh Purohit <dakshgrows@gmail.com>
2025-07-16 11:55:38 +0530

    Time: 72 ms (26.14%), Space: 77.4 MB (17.66%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,19 +1,19 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
         int n = nums.length;
-        List<Integer> temp = new ArrayList<>();
-        temp.add(nums[0]);
-        for(int i = 1; i < n; i++) {
-            if (nums[i] > temp.get(temp.size() - 1))  {
-                temp.add(nums[i]);
-            } else {
-                // Index at which the current element must be placed
-                int idx = Collections.binarySearch(temp, nums[i]);
-                if(idx < 0) idx = -(idx + 1);
-                // Place the current element in the subsequence
-                temp.set(idx, nums[i]);
+        int[][] dp = new int[n + 1][n + 1]; // dp[idx][prevIdx+1]
+
+        for (int idx = n - 1; idx >= 0; idx--) {
+            for (int prevIdx = idx - 1; prevIdx >= -1; prevIdx--) {
+                int notPick = dp[idx + 1][prevIdx + 1];
+                int pick = 0;
+                if (prevIdx == -1 || nums[idx] > nums[prevIdx]) {
+                    pick = 1 + dp[idx + 1][idx + 1];
+                }
+                dp[idx][prevIdx + 1] = Math.max(pick, notPick);
             }
         }
-        return temp.size();
+
+        return dp[0][0]; // start with idx = 0, prevIdx = -1 → shifted to 0
     }
 }
