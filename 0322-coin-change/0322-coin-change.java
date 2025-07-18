ad0940a3b8ee0024e2abf6d61c39bbe54433feda
Daksh Purohit <dakshgrows@gmail.com>
2025-07-19 00:16:40 +0530

    Time: 27 ms (27.51%), Space: 45.2 MB (29.76%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,24 +1,27 @@
 class Solution {
-    public int coinChange(int[] coins, int amount) {
-        int n = coins.length;
-        int[][] dp = new int[n][amount + 1];
-
-        // Base case: using only coins[0]
-        for (int t = 0; t <= amount; t++) {
-            if (t % coins[0] == 0) dp[0][t] = t / coins[0];
-            else dp[0][t] = (int)1e9;
-        }
-        for(int i = 1; i < n; i++) {
-            for(int t = 0; t <= amount; t++) {
-                int notTake = dp[i-1][t];
-                int take = (int)1e9;
-                if(coins[i] <= t) {
-                    take = 1 + dp[i][t-coins[i]];
-                }
-                dp[i][t] = Math.min(notTake, take);
+    public int fun(int idx, int[] coins, int amount, int[][] dp) {
+        if(idx == 0) {
+            if(amount % coins[idx] == 0) {
+                return amount / coins[0];
+            } else {
+                return (int)1e9;
             }
         }
-        int res = dp[n - 1][amount];
-        return res >= (int)1e9 ? -1 : res;
+        if(dp[idx][amount] != -1) return dp[idx][amount];
+        int notTake = fun(idx - 1, coins, amount, dp);
+        int take = Integer.MAX_VALUE;
+        if(amount >= coins[idx]) {
+            take = 1 + fun(idx, coins, amount - coins[idx], dp);
+        }
+        return dp[idx][amount] = Math.min(take, notTake);
+    }
+    public int coinChange(int[] coins, int amount) {
+        int n = coins.length;
+        int[][] dp = new int[n][amount+1];
+        for(int[] row : dp) {
+            Arrays.fill(row, -1);
+        }
+        int ans = fun(n-1, coins, amount, dp);
+        return ans == (int)1e9 ? -1 : ans;
     }
 }
