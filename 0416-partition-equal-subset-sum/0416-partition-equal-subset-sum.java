02b15b58fdb188b60243eef56a95f31c68026a3e
Daksh Purohit <dakshgrows@gmail.com>
2025-07-21 00:10:32 +0530

    Time: 98 ms (16.79%), Space: 57.7 MB (18.93%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,31 +1,32 @@
 class Solution {
-    public boolean fun(int idx, int target, int[] nums, int[][] dp) {
-        if(target == 0) return true;
-        if(idx == 0) return nums[idx] == target;
-        if(dp[idx][target] != -1) return dp[idx][target] == 1;
-        boolean notTake = fun(idx - 1, target, nums, dp);
-        boolean take = false;
-        if(target >= nums[idx]) {
-            take = fun(idx - 1, target - nums[idx], nums, dp);
+    public boolean fun(int[] nums, int idx, int target, int[][] dp) {
+        if(idx == 0) {
+            return nums[idx] == target;
         }
-        dp[idx][target] = (take || notTake) ? 1 : 0;
+        if(dp[idx][target] != -1) return dp[idx][target] == 1;
+        boolean notTake = fun(nums, idx - 1, target, dp);
+        boolean take = false;
+        if(nums[idx] <= target) {
+            take = fun(nums, idx - 1, target - nums[idx], dp);
+        }
+        dp[idx][target] = notTake || take ? 1 : 0;
         return dp[idx][target] == 1;
     }
     public boolean canPartition(int[] nums) {
-        //if the totalSum is odd -> cannot partitioned into two return false;
-        int totalSum = 0;
-        int n = nums.length; 
-        for(int i = 0; i < n; i++) {
-            totalSum += nums[i];
-        }  
-        int target = totalSum/2;
-        int[][] dp = new int[n][target+1];
+        int total = 0;
+        int n = nums.length;
+        for(int num : nums) {
+            total += num;
+        }
+        int amount = total / 2;
+        int[][] dp = new int[n][amount+1];
         for(int[] row : dp) {
             Arrays.fill(row, -1);
         }
-        if(totalSum % 2 != 0) return false;
-        else {
-            return fun(n-1, totalSum/2, nums, dp);
+        if(total % 2 != 0) {
+            return false;
+        } else {
+            return fun(nums, n-1, amount, dp);
         }
     }
 }
