7fcae3f40c3be9756a4bce1ed59185ddc765bea3
Daksh Purohit <dakshgrows@gmail.com>
2025-07-20 17:40:16 +0530

    Time: 23 ms (36.63%), Space: 50.9 MB (56.29%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.java
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.java
@@ -1,11 +1,14 @@
 class Solution {
-    public int fun(int idx1, int idx2, String text1, String text2, int[][] dp) {
-        if(idx1 < 0 || idx2 < 0) return 0;
+    public int lcsHelperFn(int idx1, int idx2, String text1, String text2, int[][] dp) {
+        if(idx1 < 0 || idx2 < 0) {
+            return 0;
+        }
         if(dp[idx1][idx2] != -1) return dp[idx1][idx2];
+        // if the el[idx1] == el[idx2]
         if(text1.charAt(idx1) == text2.charAt(idx2)) {
-            return dp[idx1][idx2] = 1 + fun(idx1-1, idx2-1, text1, text2, dp);
+            return dp[idx1][idx2] = 1 + lcsHelperFn(idx1-1, idx2-1, text1, text2, dp);
         } else {
-            return dp[idx1][idx2] = Math.max(fun(idx1-1, idx2, text1, text2, dp), fun(idx1, idx2-1, text1, text2, dp));
+            return dp[idx1][idx2] = Math.max(lcsHelperFn(idx1-1, idx2, text1, text2, dp), lcsHelperFn(idx1, idx2-1, text1, text2, dp));
         }
     }
     public int longestCommonSubsequence(String text1, String text2) {
@@ -13,8 +16,8 @@ class Solution {
         int m = text2.length();
         int[][] dp = new int[n][m];
         for(int[] row : dp) {
-            Arrays.fill(row, -1);
+            Arrays.fill(row , -1);
         }
-        return fun(n-1, m-1, text1, text2, dp);
+        return lcsHelperFn(n-1, m-1, text1, text2, dp);
     }
 }
