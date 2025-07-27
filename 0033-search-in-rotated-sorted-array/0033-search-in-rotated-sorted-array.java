51947c7c12b40dd3e68d4374eabc5654dd11134d
Daksh Purohit <dakshgrows@gmail.com>
2025-07-28 00:27:41 +0530

    Time: 0 ms (100%), Space: 42.4 MB (29.24%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -6,10 +6,18 @@ class Solution {
         while(start <= end) {
             int mid = start + (end - start) / 2;
             if(nums[mid] == target) return mid;
-            if(nums[start] <= target && target <= nums[mid]) {
-                end = mid - 1;
+            if(nums[start] <= nums[mid]) {
+                if(nums[start] <= target && target <= nums[mid]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
             } else {
-                start = mid + 1;
+                if(nums[mid] <= target && target <= nums[end]) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
             }
         }
         return -1;
