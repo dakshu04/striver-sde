97fe69c2142d7242ca34bf80411a64845e1984c7
Daksh Purohit <dakshgrows@gmail.com>
2025-07-28 11:56:44 +0530

    Time: 0 ms (100%), Space: 41.8 MB (98.53%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -14,19 +14,19 @@
  * }
  */
 class Solution {
-    private void rightSideViewHelper(TreeNode root, int level, List<Integer> list) {
+    public void rightSVHelperFn(TreeNode root, List<Integer> rightSideVList, int level) {
         if(root == null) {
             return;
         }
-        if(level == list.size()) {
-            list.add(root.val);
+        if(level == rightSideVList.size()) {
+            rightSideVList.add(root.val);
         }
-        rightSideViewHelper(root.right, level + 1, list);
-        rightSideViewHelper(root.left, level + 1, list);
+        rightSVHelperFn(root.right, rightSideVList, level + 1);
+        rightSVHelperFn(root.left, rightSideVList, level + 1);
     }
     public List<Integer> rightSideView(TreeNode root) {
-        List<Integer> result = new ArrayList<>();
-        rightSideViewHelper(root, 0, result);
-        return result;
+        List<Integer> rightSideVList = new ArrayList<>();
+        rightSVHelperFn(root, rightSideVList, 0);
+        return rightSideVList;
     }
 }
